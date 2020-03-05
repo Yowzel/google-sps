ec21204c91539a87f9ca750ff42d876401628d62
giahuy <giahuyg08@gmail.com>
2020-03-04 16:43:58 -0800

    This is me completing week 2 stuff :)

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,73 +12,44 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Adds a random greeting to the page.
- */
-function addRandomGreeting() {
-  const greetings =
-      ['I am Vietnamese', 'I\'ve vegetarian for 4 years!', 'I speak three languages', 'I go to sac state!'];
-
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
-}
-
-function getDoja() {
-  console.log('Fetching a doja cat lyric');
-
-  // The fetch() function returns a Promise because the request is asynchronous.
-  const responsePromise = fetch('/data');
-
-  // When the request is complete, pass the response into handleResponse().
-  responsePromise.then(handleResponse);
-}
-
-function handleResponse(response) {
-  console.log('Handling the response.');
-
-  // response.text() returns a Promise, because the response is a stream of
-  // content and not a simple variable.
-  const textPromise = response.text();
-
-  // When the response is converted to text, pass the result into the
-  // addQuoteToDom() function.
-  textPromise.then(addQuoteToDom);
-}
-
-/** Adds a random quote to the DOM. */
-function addQuoteToDom(test) {
-  console.log('Adding quote to dom: ' + test);
-
-  const quoteContainer = document.getElementById('quote-container');
-  quoteContainer.innerText = test;
-}
-
-function getServerStats() {
-  fetch('/data').then(response => response.json()).then((stats) => {
-    // stats is an object, not a string, so we have to
-    // reference its fields to create HTML content
-    console.log(stats.Lyric1);
-    console.log(stats.Lyric2);
-    console.log(stats.Lyric3);
-
-    const statsListElement = document.getElementById('server');
-    statsListElement.innerHTML = '';
-    statsListElement.appendChild(
-        createListElement(stats.Lyric1));
-    statsListElement.appendChild(
-        createListElement(stats.Lyric2));
-    statsListElement.appendChild(
-        createListElement(stats.Lyric3));
+// when the page is reloaded it calls this function
+function loadTasks() {
+  fetch('/list-quotes').then(response => response.json()).then((tasks) => {
+    const taskListElement = document.getElementById('quote-list');
+    tasks.forEach((task) => {
+      taskListElement.appendChild(createTaskElement(task));
+    })
   });
 }
 
-/** Creates an <li> element containing text. */
-function createListElement(text) {
-  const liElement = document.createElement('li');
-  liElement.innerText = text;
-  return liElement;
+/** Creates an element that represents a task, including its delete button. */
+function createTaskElement(task) {
+    //creating a element for the tasks/comment
+  const taskElement = document.createElement('li');
+  taskElement.className = 'task';
+
+    // making it into a html span
+  const titleElement = document.createElement('span');
+  titleElement.innerText = task.title;
+
+    // creating the delete button 
+  const deleteButtonElement = document.createElement('button');
+  deleteButtonElement.innerText = 'Delete';
+  deleteButtonElement.addEventListener('click', () => {
+    deleteTask(task);
+
+    // Remove the task from the DOM.
+    taskElement.remove();
+  });
+
+  taskElement.appendChild(titleElement);
+  taskElement.appendChild(deleteButtonElement);
+  return taskElement;
+}
+
+/** Tells the server to delete the task. */
+function deleteTask(task) {
+  const params = new URLSearchParams();
+  params.append('id', task.id);
+  fetch('/delete-quote', {method: 'POST', body: params});
 }
